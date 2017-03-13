782dc2bce0cb468d3deed371c1f1bca464fce413
Rafael Falcão <rafael.falcao@ccc.ufcg.edu.br>
2017-03-13 16:48:50 -0300

    Adicionar label 'Saldo' após o login

--- ad-extreme-master/src/main/resources/static/js/script.js
+++ ad-extreme-master/src/main/resources/static/js/script.js
@@ -1,6 +1,18 @@
 /**
  *
  */
+
+
+var disponibilzarSaldo = function() {
+	
+	var $spanSaldo = $("#saldoUser");
+	
+	$spanSaldo.append("0.0")
+
+}
+
 $(document).ready(function(){
 
+	disponibilzarSaldo();
+	
 });
