e628a61b948a1b2c350bc6fe9ea374485cab3e9c
rafaelvfalc <rafael.falcao@ccc.ufcg.edu.br>
2017-03-16 14:06:21 -0300

    Disponibilizar saldo na página logo após o login do usuário

---
+++ ad-extreme-master/src/main/java/br/edu/ufcg/computacao/si1/controller/RestAnuncioController.java
@@ -0,0 +1,50 @@
+package br.edu.ufcg.computacao.si1.controller;
+
+import br.edu.ufcg.computacao.si1.model.Anuncio;
+import br.edu.ufcg.computacao.si1.model.Usuario;
+import br.edu.ufcg.computacao.si1.service.AnuncioServiceImpl;
+import br.edu.ufcg.computacao.si1.service.UsuarioService;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by marcus on 15/03/17.
+ */
+
+@RestController
+public class RestAnuncioController {
+
+    @Autowired
+    private AnuncioServiceImpl anuncioService;
+    
+    @Autowired
+    private UsuarioService usuarioService;
+    
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public ResponseEntity<Collection<Usuario>> getUsers(){
+
+    	return new ResponseEntity<>(usuarioService.getAll(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/anuncios", method = RequestMethod.GET)
+    public ResponseEntity<Collection<Anuncio>> getPageListarAnuncios(){
+
+        return new ResponseEntity<>(anuncioService.getAll(), HttpStatus.OK);
+    }
+    
+    @RequestMapping(value = "/logged", method = RequestMethod.GET)
+    public ResponseEntity<String> getLoggedUser(){
+
+        return new ResponseEntity<>(SecurityContextHolder.getContext().getAuthentication().getName(), HttpStatus.OK);
+    }
+
+}
--- ad-extreme-master/src/main/resources/static/js/script.js
+++ ad-extreme-master/src/main/resources/static/js/script.js
@@ -2,17 +2,31 @@
  *
  */
 
-
-var disponibilzarSaldo = function() {
+function getBalanceLoggedIn() {
 	
-	var $spanSaldo = $("#saldoUser");
+	var userLoggedIn = ""
 	
-	$spanSaldo.append("0.0")
+	$.get( "/logged", function( data ) {
+		$( ".result" ).html( data );
+		userLoggedIn = data
+		
+		$.get( "/users", function( allUsers ) {
+			$( ".result" ).html( allUsers );
+			for (var i = 0; i < allUsers.length; i++) {
+				if(allUsers[i].email === userLoggedIn) {
+					var $spanSaldo = $("#saldoUser");
+				
+					$spanSaldo.append(allUsers[i].saldo + ",00")
+				}
+			}
+		});
+		
+	});
 
 }
 
 $(document).ready(function(){
-
-	disponibilzarSaldo();
+	
+getBalanceLoggedIn()
 	
 });
