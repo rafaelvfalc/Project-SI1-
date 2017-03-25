2c6581b123eaeef8389059974e9c905d53bde733
Filipe Pires <filipe.guimaraes@ccc.ufcg.edu.br>
2017-03-24 22:45:38 -0300

    alteracao das config de favoritos

--- ad-extreme-master/src/main/resources/static/js/favAnunciantes.js
+++ ad-extreme-master/src/main/resources/static/js/favAnunciantes.js
@@ -11,18 +11,30 @@ xhttp.open("GET", "/users", true);
 xhttp.send();
 
 function preencherTabela(users){
+	var listaUsers = users;
 	var resp = "";
-	var quant = users.length;
-	console.log("1 - Usuarios: "+users);
-	console.log("2 - quant: "+quant);
-	console.log("3 - first user: "+users[0]);
+	console.log("1 - Usuarios: "+listaUsers);
+	console.log("2 - quant: "+listaUsers.length);
+	console.log("3 - first user: "+listaUsers[0]);
 	if(quant > 1){
 		resp = "<tr><th style='width: 15%; text-align: center'>Anunciante</th>" +
 		"<th style='width: 20%; text-align: center'>Contato</th>" +
 		"<th style='width: 5%; text-align: center'></th></tr>";
-		for(i=0; i< quant; i++){
-			resp += "<tr><td>" + users[i].nome + "</td>";
-			resp += "<td>" + users[i].email + "</td>";
+		for(i=0; i< listaUsers.length; i++){
+			resp += "<tr><td>" + listaUsers[i].nome + "</td>";
+			resp += "<td>" + listaUsers[i].email + "</td>";
+			/*
+			 resp += "<td onClick(function(listaUsers[i].email){
+						$.post("/user/favoritos"+listaUsers[i].id), function(){
+							// atualizar pagina
+						}>";
+			if(logged.favoritos:contains(listaUsers[i].email){
+				resp += "&#10008";
+			
+			} else {
+				resp += "&#10004;"
+			}
+			*/
 			resp += "<td>" + '&#10008'+ "</td></tr>";
 		}
 	} else {
